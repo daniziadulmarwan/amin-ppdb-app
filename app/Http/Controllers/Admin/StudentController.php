18a6bfda43cb70e97f01a2e17c6048bfb632eb2f
daniziadulmarwan <zeronosvegaaltair@gmail.com>
2023-05-14 20:43:12 +0700

    add

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -42,9 +42,9 @@ class StudentController extends Controller
     {
         $data = Pendaftaran::find($id);
         $desa = Village::find($data->village);
-        // $pdf = PDF::loadView('admin.student.print', compact('data', 'desa'))->setPaper('a4', 'portrait');
-        // return $pdf->stream();
-        return view('admin.student.cetak', compact('data', 'desa'));
+        $pdf = PDF::loadView('admin.student.print', compact('data', 'desa'))->setPaper('a4', 'portrait');
+        return $pdf->stream();
+        // return view('admin.student.cetak', compact('data', 'desa'));
     }
 
     public function status(Request $request)
